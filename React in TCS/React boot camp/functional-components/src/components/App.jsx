f1af056f8e484c5bf590a88bf934be75aecdb9b1
Deepukrishnan(g023825) <deepukrishnan.r@gjensidige.com>
2021-04-09 12:22:59 +0530

    component re-use

--- React in TCS/React boot camp/functional-components/src/components/App.jsx
+++ React in TCS/React boot camp/functional-components/src/components/App.jsx
@@ -3,7 +3,7 @@ import CommentDetails from './CommentDetails.jsx'
 import ladyOne from '../avatar/lady-1.png'
 import ladyTwo from '../avatar/lady-2.png'
 import manOne from '../avatar/men-1.png'
-
+import ApprovalCard from './ApprovalCard.jsx'
 
 const comments = [
     {
@@ -31,12 +31,30 @@ const App = function() {
     return (
         <div className="ui container comments">
 
-         {
+         {/* {
             comments.map(function(value, index) {
                 return <CommentDetails key={index} image={value.image} author={value.author} time={value.time} text={value.text}/>
             })
 
-            }
+            } */}
+
+            <ApprovalCard >
+                <CommentDetails image={ladyOne} author="Anisha sam" time="Today, 7:00PM" text="Nice blog post!!! "/>
+            </ApprovalCard>
+             <ApprovalCard >
+                     Are you sure you want to continue???
+            </ApprovalCard>
+
+             <ApprovalCard>
+                 <div>
+                     <h1>Warning!!!!</h1>
+                     Are you sure you want to continue???
+                 </div>
+            </ApprovalCard>
+
+            <CommentDetails image={ladyOne} author="Anisha sam" time="Today, 7:00PM" text="Nice blog post!!! "/>
+            <CommentDetails image={ladyTwo} author="Sruthi" time="Today, 10:00PM" text="Nice blog post!!! "/>
+            <CommentDetails image={manOne} author="Deepu" time="Today, 5:00PM" text="Nice blog post!!! "/>
 
         </div>
 
--- React in TCS/React boot camp/functional-components/src/components/CommentDetails.jsx
+++ React in TCS/React boot camp/functional-components/src/components/CommentDetails.jsx
@@ -16,4 +16,22 @@ const CommentDetails = function(props) {
     )
 }
 
+/* class CommentDetails extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                <a href="/" className="avatar"><img alt="avatar" src={this.props.image}/></a>
+                <div className="content">
+                    <a href="/" className="author">{this.props.author} </a>
+                    <div className="metadata">
+                        <span className="date"> {this.props.time} </span>
+                    </div>
+                    <div className="text">{this.props.text} </div>
+                </div>
+            </div>
+
+            )
+    }
+} */
+
 export default CommentDetails;
